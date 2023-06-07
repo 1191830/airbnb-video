416581f1d975c4c396251a6192e19087726bfdfb
Rui Marques <rui.marques@foursource.com>
2023-06-07 18:36:24 +0100

    navbar

---
+++ airbnb-video/app/layout.tsx
@@ -0,0 +1,31 @@
+import { ClientOnly } from "./components/ClientOnly";
+import { Modal } from "./components/modals/Modal";
+import { Navbar } from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <Modal isOpen actionLabel="Einreichen" title="Hallo" />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  );
+}
