2d229b3ced326b90bf59c4b98c8680ebce6e6d84
Rui Marques <rui.marques@foursource.com>
2023-07-12 15:34:41 +0100

    oauth

--- airbnb-video/app/components/modals/LoginModal.tsx
+++ airbnb-video/app/components/modals/LoginModal.tsx
@@ -88,13 +88,13 @@ const LoginModal = () => {
         outline
         label="Weiter mit Google"
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => signIn('google')}
       />
       <Button
         outline
         label="Weiter mit GitHub"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn("github")}
       />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="justify-center flex flex-row items-center gap-2">
